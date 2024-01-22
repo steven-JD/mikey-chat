66a4237fc37d1f54c6709c79b84f2ba8b648b714
steven-jd <sshoemaker@januarydigital.com>
2024-01-22 13:52:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import random
 import re
 from fastapi import FastAPI, Request
 logging.basicConfig(level=logging.DEBUG)
@@ -24,34 +25,29 @@ client = OpenAI(
 
 assistant_id = 'asst_gPONKewxmSG9lXKs1W8nItPV'
 
+files =['file-phoamx3VkaccfI90aWGsxPeg',
+ 'file-IyI9IhvmAmDcz0mIKFYYSvYk',
+ 'file-hV3tlR7rWHKka1gFVm7mtYeF',
+ 'file-9iY4CtAqXgBVsVRQdmGEHKJ1']
 
-
-def run_bot(question, assistant_id):
+def run_bot(question, files):
     
-    def upload_files(client, file_paths):
-      file_ids = []
-      for path in file_paths:
-          with open(path, "rb") as file:
-              response = client.files.create(file=file, purpose='assistants')
-              file_ids.append(response.id)
-      return file_ids
+    random_number = random.randint(1, 1000)  # generates a random integer between 1 and 1000
+    assistant_name = "Marketing Wizard " + str(random_number)
 
-    file_paths = ["disdemand.pdf", "gadspec.pdf", "pmaxg.pdf", "resps.pdf"]
-    files = upload_files(client, file_paths)
-    
     assistant = client.beta.assistants.create(
-    name="Marketing Wizard",
-    description="""The Marketing Wizard Assistant will embody an analytical 
-        character, focusing on logical, data-driven responses. It will delve deeply into
-        the technical aspects of Google Ads, providing detailed, fact-based 
-        information. In scenarios where the Assistant lacks sufficient details to offer a
-        complete answer, it will prompt users for additional information. This 
-        approach ensures that responses are tailored to the specific needs of the 
-        inquiry.""",
+        name=assistant_name,
+        description="""The Marketing Wizard Assistant will embody an analytical 
+            character, focusing on logical, data-driven responses. It will delve deeply into
+            the technical aspects of Google Ads, providing detailed, fact-based 
+            information. In scenarios where the Assistant lacks sufficient details to offer a
+            complete answer, it will prompt users for additional information. This 
+            approach ensures that responses are tailored to the specific needs of the 
+            inquiry.""",
         model="gpt-4-1106-preview",
         tools=[{"type": "code_interpreter"}],
         file_ids=files
-        )
+    )
     
     thread = client.beta.threads.create(
     messages=[
@@ -90,7 +86,7 @@ def run_bot(question, assistant_id):
             )
         if run_test.status == 'completed':
             break
-        sleep(2)
+        sleep(5)
         
     messages = client.beta.threads.messages.list(
     thread_id=thread.id
@@ -171,7 +167,7 @@ async def handle_message(body, say, logger):
     output_str = re.sub(r"@\w+", "", text) 
     response = None  # Define response before the try block
     try:
-        response = run_bot(output_str, assistant_id)
+        response = run_bot(output_str, files)
     except Exception as e:
         logger.error(e)
         pass
