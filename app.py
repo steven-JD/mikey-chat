8fd93a716376c6f6a1d65c4b4d4e9016ca7f143c
steven-jd <sshoemaker@januarydigital.com>
2024-01-22 14:07:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import logging
 import os
-import random
 import re
 from fastapi import FastAPI, Request
 logging.basicConfig(level=logging.DEBUG)
@@ -24,30 +23,13 @@ client = OpenAI(
 )
 
 assistant_id = 'asst_gPONKewxmSG9lXKs1W8nItPV'
-
 files =['file-phoamx3VkaccfI90aWGsxPeg',
  'file-IyI9IhvmAmDcz0mIKFYYSvYk',
  'file-hV3tlR7rWHKka1gFVm7mtYeF',
  'file-9iY4CtAqXgBVsVRQdmGEHKJ1']
 
-def run_bot(question, files):
-    
-    random_number = random.randint(1, 1000)  # generates a random integer between 1 and 1000
-    assistant_name = "Marketing Wizard " + str(random_number)
 
-    assistant = client.beta.assistants.create(
-        name=assistant_name,
-        description="""The Marketing Wizard Assistant will embody an analytical 
-            character, focusing on logical, data-driven responses. It will delve deeply into
-            the technical aspects of Google Ads, providing detailed, fact-based 
-            information. In scenarios where the Assistant lacks sufficient details to offer a
-            complete answer, it will prompt users for additional information. This 
-            approach ensures that responses are tailored to the specific needs of the 
-            inquiry.""",
-        model="gpt-4-1106-preview",
-        tools=[{"type": "code_interpreter"}],
-        file_ids=files
-    )
+def run_bot(question, assistant_id, files):
     
     thread = client.beta.threads.create(
     messages=[
@@ -59,15 +41,19 @@ def run_bot(question, files):
     ]
     )
 
-
-
     run = client.beta.threads.runs.create(
     thread_id=thread.id,
-    assistant_id=assistant.id,
+    assistant_id=assistant_id,
     instructions="""The Google Ad Spec Assistant will embody an analytical 
     character, focusing on logical, data-driven responses. It will delve deeply into
     the technical aspects of Google Ads, providing detailed, fact-based 
-    information. 
+    information. In scenarios where the Assistant lacks sufficient details to offer a
+    complete answer, it will prompt users for additional information. This 
+    approach ensures that responses are tailored to the specific needs of the 
+    inquiry. The Assistant's primary aim is to deliver precise, comprehensive 
+    advice on Google Ads, combining its analytical nature with a user-friendly 
+    approach to seeking clarification, thereby enhancing the accuracy and 
+    relevance of its assistance. 
     """,
     tools=[{"type": "code_interpreter"}, {"type": "retrieval"}]
     )
@@ -80,7 +66,7 @@ def run_bot(question, files):
             )
         if run_test.status == 'completed':
             break
-        sleep(5)
+        sleep(2)
         
     messages = client.beta.threads.messages.list(
     thread_id=thread.id
@@ -154,14 +140,14 @@ async def handle_message(body, say, logger):
     channel_id = event.get("channel")
     user_id = event.get("user")
     text = event.get("text")
-    context_str = "Be as detailed as possible in your response. Use single * when bolding an item"
+    context_str = "Be as detailed as possible in your response. Ensure you reference the uploaded documents in your response.  Use these markdown guidelines when formatting your response: When using bold in your responses, use the format *text*, when using bullet points you can mimic the formatting using the bullet point symbol (•) and a space before your text (• bulleted list), Ensure you don't use ** around texts, only * for bold. You should ensure your responses are extremely detailed - not just a few words."
     if text is None:
       text = ""
       text += context_str
-    output_str = re.sub(r"@\w+", "", text) 
+    output_str = "Be as detailed as possible in your response. Ensure you reference the uploaded documents in your response.  Use these markdown guidelines when formatting your response: When using bold in your responses, use the format *text*, when using bullet points you can mimic the formatting using the bullet point symbol (•) and a space before your text (• bulleted list), Ensure you don't use ** around texts, only * for bold." + ": " + re.sub(r"@\w+", "", text) 
     response = None  # Define response before the try block
     try:
-        response = run_bot(output_str, files)
+        response = run_bot(output_str, assistant_id, files)
     except Exception as e:
         logger.error(e)
         pass
@@ -169,18 +155,6 @@ async def handle_message(body, say, logger):
     if response:  # Check if response is not None before accessing it
         result = response
         # If the user sends a DM to the bot, the bot will respond with a message.
-        await say(
-        channel=channel_id,
-        blocks=[
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": result
-                }
-            }
-        ],
-        text=result  # This is a fallback for notifications and accessibility
-    )
+        await say(channel=channel_id, text=result)  
         
 
